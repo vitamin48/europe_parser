da12c2dcc6db9d4305109d26338f2b9a5d9c161c
Компьютер <vitamin48>
2024-05-06 22:37:27 +0300

    Доделано

--- JsonToXLS.py
+++ JsonToXLS.py
@@ -22,6 +22,19 @@ def read_bad_brand():
     return set(brands)
 
 
+def transform_price(x):
+    result = x * 5 if x < 200 else (
+        x * 4.5 if 200 <= x < 500 else (
+            x * 4 if 500 <= x < 1000 else (
+                x * 3.5 if 1000 <= x < 5000 else (
+                    x * 3 if 5000 <= x < 10000 else (
+                        x * 2.5 if 10000 <= x < 20000 else (x * 2))))))
+    # Убеждаемся, что значение после преобразований не меньше 490
+    result = max(result, 490)
+    # Округление до целого числа
+    return round(result)
+
+
 def create_df_by_dict(data_dict):
     rows = []
 
@@ -46,13 +59,13 @@ def create_df_by_dict(data_dict):
             img_url2 = "-"
         # Подготовка строки
         row = {
-            "Артикул": key,
+            "ArtNumber": key,
             "Название": value.get("name", ""),
-            "Цена": value.get("price", ""),
+            "Цена Европы": value.get("price", ""),
             "Остатки": value.get("stock", ""),
             "Описание": value.get("description", ""),
-            "img_url1": img_url1,
-            "img_url2": img_url2,
+            "Ссылка на главное фото товара": img_url1,
+            "Ссылки на другие фото товара": img_url2,
             "art_url": value.get("art_url", ""),
             "Бренд": brand,
             "Страна": country,
@@ -72,9 +85,46 @@ def create_df_by_dict(data_dict):
     df["Бренд_нижний_регистр"] = df["Бренд"].str.lower().str.strip()
     df_filtered = df[~df["Бренд_нижний_регистр"].isin(excluded_brands)].copy()
     df_filtered.drop(columns=["Бренд_нижний_регистр"], inplace=True)
-    print()
+    df_filtered["Артикул"] = df_filtered["ArtNumber"].apply(lambda art: f'e_{art}')
+    # Добавляем столбец Цена для OZON
+    df_filtered['Цена для OZON'] = df_filtered['Цена Европы'].apply(transform_price)
+    # Добавляем столбец Цена до скидки
+    df_filtered['Цена до скидки'] = df_filtered['Цена для OZON'].apply(lambda x: int(round(x * 1.3)))
+    # Добавляем столбец НДС Не облагается
+    df_filtered["НДС"] = "Не облагается"
+    # Задаем порядок столбцов
+    desired_order = ['Артикул', 'Название', 'Цена для OZON', 'Цена до скидки', 'НДС', 'Цена Европы', 'Ширина, мм',
+                     'Высота, мм', 'Длина, мм', 'Ссылка на главное фото товара', 'Ссылки на другие фото товара',
+                     'Бренд', 'ArtNumber', 'Описание', 'Страна', 'art_url']
+    result_df = df_filtered[desired_order]
+    return result_df
+
+
+def create_xls(df):
+    file_name = f'out\\Europe.xlsx'
+    # Сохранение DataFrame в Excel с использованием Styler
+    with pd.ExcelWriter(file_name, engine='openpyxl') as writer:
+        df.to_excel(writer, sheet_name='OZON', index=False, na_rep='NaN')
+        # Установка ширины столбцов
+        worksheet_ozon = writer.sheets['OZON']
+        for column in df:
+            column_width = max(df[column].astype(str).map(len).max(), len(column)) + 2
+            col_letter = get_column_letter(df.columns.get_loc(column) + 1)
+            worksheet_ozon.column_dimensions[col_letter].width = column_width
+        # Закрепите первую строку
+        worksheet_ozon.freeze_panes = 'A2'
+        # Корректировка ширины столбцов
+        worksheet_ozon.column_dimensions[get_column_letter(df.columns.get_loc('Название') + 1)].width = 30
+        worksheet_ozon.column_dimensions[get_column_letter(df.columns.get_loc('Описание') + 1)].width = 30
+        worksheet_ozon.column_dimensions[
+            get_column_letter(df.columns.get_loc('Ссылка на главное фото товара') + 1)].width = 30
+        worksheet_ozon.column_dimensions[
+            get_column_letter(df.columns.get_loc('Ссылки на другие фото товара') + 1)].width = 30
+        worksheet_ozon.column_dimensions[
+            get_column_letter(df.columns.get_loc('art_url') + 1)].width = 20
 
 
 if __name__ == '__main__':
     data_json = read_json()
-    create_df_by_dict(data_dict=data_json)
+    df = create_df_by_dict(data_dict=data_json)
+    create_xls(df)
